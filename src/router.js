deba7372e653ac3207b1fc6f18b63ed02cf7953e
brunolgb <brunodscolado@gmail.com>
2020-04-22 17:22:07 -0400

    remove signin

--- src/router.js
+++ src/router.js
@@ -4,7 +4,6 @@ import {Switch, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Box from './pages/Box';
 import BoxSpecific from './pages/BoxSpecific';
-import Signin from './pages/Signin';
 import NotFound from './pages/NotFound';
 
 export default function RouterPage(){
@@ -14,7 +13,6 @@ export default function RouterPage(){
             <Route path="/search/:search" component={Home}/>
             <Route exact path="/box" component={Box}/>
             <Route path="/box/:product/:price" component={BoxSpecific}/>
-            <Route path="/signin" component={Signin}/>
             <Route path="*" component={NotFound}/>
         </Switch>
     )
