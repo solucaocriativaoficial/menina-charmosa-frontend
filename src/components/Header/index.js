b4e28e152f6733b02213bbe76a63990fbe1fe630
brunolgb <brunodscolado@gmail.com>
2020-04-22 17:09:04 -0400

    search redirect in home, box count, signin modal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,83 @@
 import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import Logo from '../../assets/icon.svg';
 import iconSearch from '../../assets/icon-search.svg';
 import iconCart from '../../assets/icon-car.svg';
+import Api from '../../services/api';
+import HeaderApi from '../../services/header';
+import {removeToken, getToken} from '../Authenticate';
+import SignIn from '../Modals/Signin';
 
-export default function Header(){
+export default function Header({title = "Menina Charmosa"}){
     const [openMenu, setOpenMenu] = useState('');
     const [showMenu, setShowMenu] = useState('');
+    const [boxData, setBoxData] = useState('x');
+    const [handleBox, setHandleBox] = useState(0);
+    const [handleSearch, setHandleSearch] = useState('');
+    const [dataSearch, setDataSearch] = useState([]);
+    const [showResults, setShowResults] = useState('');
+    const [logout, setLogout] = useState(false);
+    const History = useHistory();
+
+    document.title = title
+
+    useEffect(() => {
+        async function find(){
+            try {
+                const countBox = await Api.get('/box/count/', HeaderApi);
+                const {success, content} = countBox.data;
+                if(success)
+                setBoxData(content);
+            } catch (error) {
+                setBoxData(error.response.data.content)
+            }
+        }
+
+        if(getToken() !== null)
+        find();
+
+        setTimeout(()=>{
+            let count = handleBox;
+            setHandleBox(count + 1)
+        }, 5000);
+    }, [handleBox])
+
+    useEffect(() => {
+        async function search(){
+            try {
+                const searchFilter = handleSearch === '' ? '' : `?search=${handleSearch}`;
+                const fieldSearch = await Api.get(`/product${searchFilter}`, HeaderApi);
+                const {success, content} = fieldSearch.data;
+                if(success)
+                setDataSearch(content);
+            } catch (error) {
+                console.log(error.response.data.message)
+            }
+        }
+        if(getToken() !== null)
+        search();
+    }, [handleSearch, History])
 
     useEffect(() => {
         openMenu === '' ? setShowMenu('') : setShowMenu('menu-links-moviment')
     }, [openMenu])
+
+    setTimeout(() => {
+        if(getToken() === null)
+        setLogout(true);
+    }, 1000);
+
+    function changeHandleSearch(event){
+        const valueInputSearch = event.target.value;
+        setHandleSearch(valueInputSearch);
+        if(valueInputSearch === '')
+        History.push('/')
+    }
+
+    const desconect = () => {
+        removeToken();
+        setLogout(true);
+    }
 
     return(
         <header id="header-main">
@@ -25,13 +92,25 @@ export default function Header(){
                 <div id="box-field-search-cart">
                     <div id="input-search">
                         <img src={iconSearch} alt=""/>
-                        <input type="text" name="search"/>
+                        <input type="text" name="search" value={handleSearch} onFocus={() => setShowResults("search-results-moviment")} onChange={changeHandleSearch}/>
+                    </div>
+                    <div className={`search-results ${showResults}`}>
+                        {
+                            handleSearch === '' ? '' :
+                            dataSearch.map((product) => (
+                                <div className="search-results-content" key={product.id} onClick={() => {
+                                    History.push(`/search/${product.product_name}`)
+                                    setShowResults('')
+                                }}>{product.product_name}</div>
+                            ))
+                        }
                     </div>
                 </div>
                 <div id="box-menu-links">
                     <span id="cart">
                         <Link to="/box" title="Minha caixinha">
                             <img src={iconCart} alt="Caixinha"/>
+                            <div>{boxData}</div>
                         </Link>
                     </span>
                     <div className="btn-menu" onClick={() => {
@@ -46,14 +125,20 @@ export default function Header(){
                     <div className="transparent"></div>
                     <nav className={`menu-links ${showMenu}`}>
                         <span>
-                            <Link to="/contact">Contato</Link>
-                        </span>
-                        <span>
                             <Link to="/box">Minha caixinha</Link>
+                        </span>
+                        <span onClick={() => desconect()}>
+                            <a href="logout" onClick={(e) => {
+                                e.preventDefault()
+                            }}>Sair</a>
                         </span>
                     </nav>
                 </div>
             </div>
+            {
+                !logout ? null : 
+                <SignIn onCloseModalSignIn={() => setLogout(false)}/>
+            }
         </header>
     );
 }
---
+++ src/components/Modals/Signin.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import Api from '../../services/api';
+import { Form } from '@unform/web';
+import Input from '../forms/Input';
+import {addToken} from '../Authenticate';
+import '../../styles/Main.css';
+import imageclose from '../../assets/icon-close.svg';
+
+export default function SignIn({onCloseModalSignIn = () => {}}){
+    const [loading, setLoading] = useState(false);
+    const [message, setMessage] = useState('');
+    const History = useHistory();
+
+    const checkSignin = async (data) => {
+        try {
+            const authenticate = await Api.post("/signin", data);
+            const {success, content, message: messageResponse} = authenticate.data;
+            if(success)
+            {
+                addToken(content)
+            }
+
+            else
+            setMessage(messageResponse)
+        } catch (error) {
+            setMessage(error.response.data.content)
+        }
+    }
+
+    const handleSubmit = async (data) => {
+        if(data.mail.indexOf("@") < 1)
+        setMessage("E-mail informado é inválido! Veja o formato dele!")
+
+        else{
+            setLoading(true);
+            await checkSignin(data);
+            History.goBack();
+        }
+    }
+    return(
+        <div className="modal-information-auth">
+            <section className="content-modal auth">
+                <button className="btn-close-modal" onClick={() => onCloseModalSignIn()}>
+                    <img src={imageclose} alt="Botão de fechar"/>
+                </button>
+                <h1>Faça seu login</h1>
+                <Form onSubmit={handleSubmit}>
+                    <div className="auth-field">
+                        <label htmlFor="mail">
+                            E-mail
+                        </label>
+                        <Input type="text" name="mail" id="mail" maxLength="200"/>
+                    </div>
+                    <div className="auth-field">
+                        <label htmlFor="mail">
+                            Password
+                        </label>
+                        <Input type="password" name="password" id="password"/>
+                    </div>
+                    <div>{message}</div>
+                    <button type="submit">{loading ? "Carregando!" : "Entrar"}</button>
+                </Form>
+                <button>Cadastrar-se</button>
+            </section>
+        </div>
+    )
+}
