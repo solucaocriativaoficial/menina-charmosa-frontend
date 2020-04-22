f493b913302a360ac341b436a6ca55ba47383888
brunolgb <brunodscolado@gmail.com>
2020-04-22 17:10:02 -0400

    footer com copyright and name by soluction creative

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function Footer(){
+    return(
+        <footer className="footer-main">
+            <div className="container">
+                Solução Criativa &copy; Todos os direitos reservados
+            </div>
+        </footer>
+    )
+}
